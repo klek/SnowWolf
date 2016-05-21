6a7d21da601beb2d2b41ff93bef0d605624bc6b4
klek <a.tobias.pettersson@gmail.com>
2016-05-21 17:46:21 +0200

    Trying to get a window

--- code/sdl_SnowWolf.cpp
+++ code/sdl_SnowWolf.cpp
@@ -1,7 +1,32 @@
 #include <SDL.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "SnowWolf", "This is SnowWolf", 0);
+	// Previously used messagebox for SDL
+//	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "SnowWolf", "This is SnowWolf", 0);
+
+
+	// Variables
+	SDL_Window *Window;
+
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		// Todo: SDL_Init didn't work!
+		fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	// Setting up the SDL window
+	Window = SDL_CreateWindow("SnowWolf",
+				  SDL_WINDOWPOS_UNDEFINED,
+				  SDL_WINDOWPOS_UNDEFINED,
+				  640,
+				  480,
+				  SDL_WINDOW_RESIZABLE);
+
+
+	// Shutdown SDL
+	SDL_Quit();
+
 	return 0;
 }
