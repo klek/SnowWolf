0f1dfc9a153c9d9f75f05fbea79a525b8f435b6d
klek <tobias90.pettersson@telia.com>
2016-09-10 13:48:59 +0200

    Switched to GLFW instead of SDL, tested with  a GLFW test program

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 This is SnowWolf. A programming project with graphics on Linux.
 
 To get SDL working I needed to install libsdl2-dev and libsdl2-dbg.
+
+Switched from SDL to GLFW for graphics. To get started I followed GLFW doc and
+http://stackoverflow.com/questions/17768008/how-to-build-install-glfw-3-and-use-it-in-a-linux-project
--- code/build.sh
+++ code/build.sh
@@ -2,5 +2,7 @@
 
 mkdir -p ../build
 pushd ../build
-c++ ../code/sdl_SnowWolf.cpp -o SnowWolf -g `sdl2-config --cflags --libs`
+g++ ../code/sdl_SnowWolf.cpp -o SnowWolf -g `sdl2-config --cflags --libs`
+g++ ../code/glfw_SnowWolf.cpp -o SnowWolf_glfw -lGL -lGLU -lglfw3 -lX11 -lXxf86vm -lXrandr -lpthread -ldl \
+-lXinerama -lXcursor
 popd
